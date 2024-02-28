3bf8cd3bd7b72d44cfedfa9fbfbc85d52fe503bc
Akbar <akbar@WIN-8GQFEQRIH1S.>
2024-02-29 00:09:13 +0500

    toggleterm win/linux

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -6,9 +6,17 @@ local powershell_options = {
 	shellquote = "",
 	shellxquote = "",
 }
+-- if system is windows then use powershell
+if vim.fn.has("win32") == 1 then
+	if vim.fn.executable("pwsh") == 1 then
+		powershell_options.shell = "pwsh"
+	else
+		powershell_options.shell = "powershell"
+	end
 
-for option, value in pairs(powershell_options) do
-	vim.opt[option] = value
+	for option, value in pairs(powershell_options) do
+		vim.opt[option] = value
+	end
 end
 
 require("toggleterm").setup({
@@ -20,7 +28,7 @@ require("toggleterm").setup({
 		winblend = 0,
 		highlights = {
 			border = "Normal",
-            background = "Normal",
+			background = "Normal",
 		},
 	},
 	size = function(term)
@@ -36,7 +44,7 @@ require("toggleterm").setup({
 	hide_numbers = true, -- hide the number column in toggleterm buffers
 	shade_filetypes = {},
 	shade_terminals = true,
-    shading_factor = 1, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+	shading_factor = 1, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
 	start_in_insert = true,
 	close_on_exit = true, -- close the terminal window when the process exits
 })
