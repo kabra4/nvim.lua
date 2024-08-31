c131fc3b7f0a2b28bedb306ec83ffef33692ae5f
Akbar <57127182+kabra4@users.noreply.github.com>
2024-08-31 14:03:20 +0500

    theme changes

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,12 +1,13 @@
 function ColorMyPencils(color)
-	color = color or "catppuccin-mocha"
+	--color = color or "catppuccin-mocha"
+	color = color or "kanagawa"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
-    vim.api.nvim_set_hl(0, "CursorLine", { ctermbg = 235, bg = "#2c323c" })
-    vim.api.nvim_set_hl(0, "CursorColumn", { ctermbg = 236, bg = "#323840" })
+	vim.api.nvim_set_hl(0, "CursorLine", { ctermbg = 235, bg = "#2c323c" })
+	vim.api.nvim_set_hl(0, "CursorColumn", { ctermbg = 236, bg = "#323840" })
 end
 
 ColorMyPencils()
